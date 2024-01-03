a92758335f6b6e4012f95fac597455700f11dd49
Luisru9 <lrgp42@outlook.com>
2024-01-03 09:09:47 -0400

    first part of the code

---
+++ opcode.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ *push - adds to the stack
+ *@stack:a pointer to the head of the stack
+ *@line_number:data to be stored
+ */
+
+void push(m_stack_t **stack, unsigned int line_number)
+{
+
+}
